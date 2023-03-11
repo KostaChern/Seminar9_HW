073453e101ce345798ec1041d76bc49a384887fc
Konstantin Chern <codahl@yandex.ru>
2023-03-11 21:38:38 +0300

    Task1 Solved

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,3 +2,32 @@
 // все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
+
+// 
+
+Console.Clear();
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Четные числа в диапазоне от {m} до {n} =>");
+
+EvenNambersFromMtoN(m, n);
+
+int EvenNambersFromMtoN(int m, int n)
+{
+    if (m > n) return 0;
+    else
+    {
+        if (m % 2 == 0)
+        {
+            Console.Write($"{m}, ");
+        }
+        m++;
+        EvenNambersFromMtoN(m, n);
+        return m;
+    }
+}
