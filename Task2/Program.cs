1fcefda833b177e40795976e31316fe0fa903e39
Konstantin Chern <codahl@yandex.ru>
2023-03-11 22:32:20 +0300

    Task2 Solved

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,3 +3,33 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30
+
+Console.Clear();
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumNumbersFromMtoN(m, n);
+
+void SumNumbersFromMtoN(int m, int n)
+{
+    Console.WriteLine();
+    Console.Write($"Cумма чисел в заданном диапазоне => {SumNumbMN(m - 1, n)}");
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+int SumNumbMN(int m, int n)
+{
+    int resalt = 0;
+    if (m == n) return 0;
+    else
+    {
+        m++;
+        resalt = m + SumNumbMN(m, n);
+        return resalt;
+    }
+}
